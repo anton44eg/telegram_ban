f0ebb6461b20f78aca3e773a7b71e931238b940a
Anton Simernia <anton.simernya@gmail.com>
2022-03-03 10:21:39 -0800

    update channels

--- normalize_channels.py
+++ normalize_channels.py
@@ -1,22 +1,17 @@
 import asyncio
-from contextlib import suppress
-import logging
-import os
-import random
 
 import funcy
-from pyrogram import Client
-from pyrogram.errors import UsernameInvalid, UsernameNotOccupied
-from pyrogram.raw.functions.messages import Report
-from pyrogram.raw.types import InputReportReasonViolence
 
 
 async def main():
-    with open('chats.txt') as chats_file:
+    with open('channels.txt') as chats_file:
         chat_names = funcy.lfilter(None, chats_file.readlines())
-        chat_names = list(set(chat_names))
-        for chat_name in chat_names:
-            chat_name = chat_name.removeprefix('https://t.me/').removeprefix('@').strip()
-            print(chat_name)
+    chat_names = list(set(chat_names))
+    normalized_names = []
+    for chat_name in chat_names:
+        chat_name = chat_name.removeprefix('https://t.me/').removeprefix('@').strip()
+        normalized_names.append(chat_name)
+    for name in sorted(set(normalized_names)):
+        print(name)
 
 asyncio.run(main())
